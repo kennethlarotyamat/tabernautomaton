0eeb3a3e55597c9e57839dceb1c61d13b2bfefe8
Kenneth Larot Yamat <kenneth.larot.yamat@gmail.com>
2024-12-16 05:14:36 -0800

    1

---
+++ javascript/indexTabernautomaton.js
@@ -0,0 +1,234 @@
+
+// iframe document swap START
+
+document.getElementById('changeSrcBtn').addEventListener('click', function() {
+var newSource = 'https://kennethlarotyamat.github.io/media/text/fmop10mREADME.md'; // Replace this URL with your desired source
+
+var iframe = document.getElementById('myIframe');
+iframe.src = newSource;
+
+var newHeadingText = "Project 13 Readme";
+document.getElementById("documentHeader").innerText = newHeadingText;
+
+});
+
+document.getElementById('changeSrcBtn2').addEventListener('click', function() {
+var newSource2 = 'https://kennethlarotyamat.github.io/media/text/indexTranslationsReadme.md'; // Replace this URL with your desired source
+
+var iframe = document.getElementById('myIframe');
+iframe.src = newSource2;
+var newHeadingText2 = "Development Commentary";
+document.getElementById("documentHeader").innerText = newHeadingText2;
+});
+
+document.getElementById('changeSrcBtn3').addEventListener('click', function() {
+var newSource3 = 'https://kennethlarotyamat.github.io/media/text/readme.md'; // Replace this URL with your desired source
+
+var iframe = document.getElementById('myIframe');
+iframe.src = newSource3;
+var newHeadingText3 = "Project 16 Readme";  // Replace this Title with your desired Title
+document.getElementById("documentHeader").innerText = newHeadingText3;
+});
+
+// iframe document swap END
+
+// Spanish to English Buttons START
+
+document.getElementById('changeSrcBtn3s1').addEventListener('click', function() {
+var newSource3s1 = 'https://kennethlarotyamat.github.io/media/text/translationFiles/rds1.md'; // Replace this URL with your desired source
+
+var iframe = document.getElementById('myIframe');
+iframe.src = newSource3s1;
+var newHeadingText3s1 = "Rubén Darío";  // Replace this Title with your desired Title
+document.getElementById("documentHeader").innerText = newHeadingText3s1;
+});
+
+
+document.getElementById('changeSrcBtn3s2').addEventListener('click', function() {
+var newSource3s2 = 'https://kennethlarotyamat.github.io/media/text/translationFiles/cvs2.md'; // Replace this URL with your desired source
+
+var iframe = document.getElementById('myIframe');
+iframe.src = newSource3s2;
+var newHeadingText3s2 = "José de Espronceda";  // Replace this Title with your desired Title
+document.getElementById("documentHeader").innerText = newHeadingText3s2;
+});
+
+
+document.getElementById('changeSrcBtn3s3').addEventListener('click', function() {
+var newSource3s3 = 'https://kennethlarotyamat.github.io/media/text/translationFiles/pns3.md'; // Replace this URL with your desired source
+
+var iframe = document.getElementById('myIframe');
+iframe.src = newSource3s3;
+var newHeadingText3s3 = "Antonio Machado";  // Replace this Title with your desired Title
+document.getElementById("documentHeader").innerText = newHeadingText3s3;
+});
+
+
+document.getElementById('changeSrcBtn3s4').addEventListener('click', function() {
+var newSource3s4 = 'https://kennethlarotyamat.github.io/media/text/translationFiles/mdus4.md'; // Replace this URL with your desired source
+
+var iframe = document.getElementById('myIframe');
+iframe.src = newSource3s4;
+var newHeadingText3s4 = "Miguel de Unamuno";  // Replace this Title with your desired Title
+document.getElementById("documentHeader").innerText = newHeadingText3s4;
+});
+
+document.getElementById('changeSrcBtn3s5').addEventListener('click', function() {
+var newSource3s5 = 'https://kennethlarotyamat.github.io/media/text/translationFiles/jrs5.md'; // Replace this URL with your desired source
+
+var iframe = document.getElementById('myIframe');
+iframe.src = newSource3s5;
+var newHeadingText3s5 = "José Rizal";  // Replace this Title with your desired Title
+document.getElementById("documentHeader").innerText = newHeadingText3s5;
+});
+
+// Spanish to English Buttons END
+
+// English to Spanish Buttons Start
+
+document.getElementById('changeSrcBtn3e1').addEventListener('click', function() {
+var newSource3e1 = 'https://kennethlarotyamat.github.io/media/text/translationFiles/wcwe1.md '; // Replace this URL with your desired source
+
+var iframe = document.getElementById('myIframe');
+iframe.src = newSource3e1;
+var newHeadingText3e1 = "William Carlos Williams";  // Replace this Title with your desired Title
+document.getElementById("documentHeader").innerText = newHeadingText3e1;
+});
+
+document.getElementById('changeSrcBtn3e2').addEventListener('click', function() {
+var newSource3e2 = 'https://kennethlarotyamat.github.io/media/text/translationFiles/tsee2.md'; // Replace this URL with your desired source
+
+var iframe = document.getElementById('myIframe');
+iframe.src = newSource3e2;
+var newHeadingText3e2 = "T. S. Eliot";  // Replace this Title with your desired Title
+document.getElementById("documentHeader").innerText = newHeadingText3e2;
+});
+
+document.getElementById('changeSrcBtn3e3').addEventListener('click', function() {
+var newSource3e3 = 'https://kennethlarotyamat.github.io/media/text/translationFiles/wse3.md'; // Replace this URL with your desired source
+
+var iframe = document.getElementById('myIframe');
+iframe.src = newSource3e3;
+var newHeadingText3e3 = "Wallace Stevens";  // Replace this Title with your desired Title
+document.getElementById("documentHeader").innerText = newHeadingText3e3;
+});
+
+document.getElementById('changeSrcBtn3e4').addEventListener('click', function() {
+var newSource3e4 = 'https://kennethlarotyamat.github.io/media/text/translationFiles/eece4.md '; // Replace this URL with your desired source
+
+var iframe = document.getElementById('myIframe');
+iframe.src = newSource3e4;
+var newHeadingText3e4 = "E. E. Cummings";  // Replace this Title with your desired Title
+document.getElementById("documentHeader").innerText = newHeadingText3e4;
+});
+
+//  English to Spanish Buttons END 
+
+// Contact Form Buttons START
+
+document.getElementById('changeSrcBtnCF1').addEventListener('click', function() {
+var newSource3e41 = 'https://kennethlarotyamat.github.io/html/newslettersignup2.html'; // Replace this URL with your desired source
+
+var iframe = document.getElementById('myIframe');
+iframe.src = newSource3e41;
+var newHeadingText3e41 = "Newsletter Sign-Up 2";  // Replace this Title with your desired Title
+document.getElementById("documentHeader").innerText = newHeadingText3e41;
+});
+
+document.getElementById('changeSrcBtnCF2').addEventListener('click', function() {
+var newSource3e42 = 'https://kennethlarotyamat.github.io/html/newslettersignup.html'; // Replace this URL with your desired source
+
+var iframe = document.getElementById('myIframe');
+iframe.src = newSource3e42;
+var newHeadingText3e42 = "Newsletter Sign-Up";  // Replace this Title with your desired Title
+document.getElementById("documentHeader").innerText = newHeadingText3e42;
+});
+
+document.getElementById('changeSrcBtnCF3').addEventListener('click', function() {
+var newSource3e43 = 'https://kennethlarotyamat.github.io/html/project11contactform.html'; // Replace this URL with your desired source
+
+var iframe = document.getElementById('myIframe');
+iframe.src = newSource3e43;
+var newHeadingText3e43 = "Project 11 Contact Form";  // Replace this Title with your desired Title
+document.getElementById("documentHeader").innerText = newHeadingText3e43;
+});
+
+document.getElementById('changeSrcBtnCF4').addEventListener('click', function() {
+var newSource3e44 = 'https://kennethlarotyamat.github.io/html/project9contactform.html'; // Replace this URL with your desired source
+
+var iframe = document.getElementById('myIframe');
+iframe.src = newSource3e44;
+var newHeadingText3e44 = "Initial Contact Form";  // Replace this Title with your desired Title
+document.getElementById("documentHeader").innerText = newHeadingText3e44;
+});
+
+// Contact Form Buttons END
+
+// Misc Translation Start
+
+document.getElementById('changeSrcBtn3e4MT').addEventListener('click', function() {
+    var newSource3e44mt = 'https://kennethlarotyamat.github.io/project16/media/text/translationFiles/englishMisc.md'; // Replace this URL with your desired source
+    
+    var iframe = document.getElementById('myIframe');
+    iframe.src = newSource3e44mt;
+    var newHeadingText3e44mt = "Miscellaneous Translations";  // Replace this Title with your desired Title
+    document.getElementById("documentHeader").innerText = newHeadingText3e44mt;
+    });
+
+    document.getElementById('changeSrcBtn3s5MT').addEventListener('click', function() {
+        var newSource3e44mts = 'https://kennethlarotyamat.github.io/media/text/translationFiles/spanishMisc.md'; // Replace this URL with your desired source
+        
+        var iframe = document.getElementById('myIframe');
+        iframe.src = newSource3e44mts;
+        var newHeadingText3e44mts = "Miscellaneous Translations";  // Replace this Title with your desired Title
+        document.getElementById("documentHeader").innerText = newHeadingText3e44mts;
+        });
+
+// Misc Translation Start        
+
+// Modal shutoff START
+
+$(function(){
+$("body").on('hidden.bs.modal', function (e) {
+var $iframes = $(e.target).find("iframe");
+$iframes.each(function(index, iframe){
+$(iframe).attr("src", $(iframe).attr("src"));
+});
+});
+});  
+$(function() {
+"use strict";
+
+// Modal shutoff END
+
+// Grayscale Toggle START
+
+let bodyA = $(".bodya");
+let footer = $(".footer");
+let main = $(".main");
+let aside = $("aside");
+let subheadercenterheadertextbutton = $(".subheadercenterheadertextbutton");
+let header = $(".header");
+let contentouter = $(".contentouter");
+let modeToggle = $("#mode-toggle");
+let modeStatus = $(".mode-status");
+
+function toggleMode() {
+bodyA.toggleClass("dark-modefields");
+footer.toggleClass("dark-modefields");
+main.toggleClass("dark-modefields");
+aside.toggleClass("dark-modefields");
+subheadercenterheadertextbutton.toggleClass("dark-modefields");
+header.toggleClass("dark-modefields");
+contentouter.toggleClass("dark-mode");
+
+const modeMessage = contentouter.hasClass("dark-mode") ?
+"Grayscale" : "Color";
+
+modeStatus.text(modeMessage);
+}
+
+modeToggle.click(toggleMode);
+});
+
+// Grayscale Toggle END
